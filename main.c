e9398c2db3c8f8dcbc5d303041e5d92b3d47e074
Harikrishnan <harikrishnan.prabakaran@zohocorp.com>
2017-09-14 14:42:12 +0530

    Add main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  userspacercu
+//
+//  Created by hari on 14/09/17.
+//  Copyright © 2017 hari. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
